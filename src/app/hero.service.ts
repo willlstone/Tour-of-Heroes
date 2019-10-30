6f6958f26c31eb5278c42e78dca538cd0db4a661
willlstone <stonew2@rpi.edu>
2019-10-30 14:39:15 -0400

    Message Service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,13 +3,16 @@ import { Observable, of} from 'rxjs';
 
 import { Hero} from './hero';
 import { HEROES} from './mock-heroes';
+import { MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message after fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
