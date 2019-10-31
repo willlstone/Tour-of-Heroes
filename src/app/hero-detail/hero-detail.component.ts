4fb4f8f1eb94ba8e09ddbb8e0eb856c14b30393b
willlstone <stonew2@rpi.edu>
2019-10-31 15:31:37 -0400

    Search/Add/Delete Functionality

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,24 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Hero} from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import {ActivatedRoute} from '@angular/router';
-import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
+
   constructor(
     private route: ActivatedRoute,
-    // Holds info about the route in this instance of the component. interested in the route's parameters extracted from the URL
     private heroService: HeroService,
     private location: Location
-    // Angular service interacting with browser. Used later ot navigate back to view that navigated here
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.getHero();
@@ -29,7 +28,13 @@ export class HeroDetailComponent implements OnInit {
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
+
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
   }
 }
